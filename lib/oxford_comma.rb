d5cbaf9d83433e8aad45d9a26e722ebd3d188b7b
Michael <mikahado@gmail.com>
2023-02-14 15:48:00 -0500

    commit tree 6a9b39b682c25a4e1ce8f20da6455754db9cfc77

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
 
+    if array.length > 2
+        last_element = array.pop
+        with_commas = array.join(", ")
+        "#{with_commas}, and #{last_element}"
+    else 
+        array.join(" and ")
+    end
+
 end
